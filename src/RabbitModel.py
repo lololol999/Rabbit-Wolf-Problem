d264eb6a36439a1c9cd64d9dfae35add7b99a651
lololol999 <stepanbaranov999@gmail.com>
2025-09-13 16:26:12 +0800

    StupidAI

--- src/RabbitModel.py
+++ src/RabbitModel.py
@@ -1,7 +1,18 @@
-import torch
-from torch.autograd import variable as Variable
+import torch.nn as nn
+import torch.nn.functional as F
 
-x = torch.tensor(torch.ones(2, 2) * 2, device = 'cuda' if torch.cuda.is_available() else 'cpu', requires_grad=True)
-z = 2 * (x * x) + 5 * x
-z.backward(torch.ones(2, 2))
-print(x.grad)
+class Net(nn.Module):
+   def __init__(self):
+       super(Net, self).__init__()
+       self.fc1 = nn.Linear(28 * 28, 200)
+       self.fc2 = nn.Linear(200, 200)
+       self.fc3 = nn.Linear(200, 10)
+
+   def forward(self, x):
+       x = F.relu(self.fc1(x))
+       x = F.relu(self.fc2(x))
+       x = self.fc3(x)
+       return F.log_softmax(x)
+
+net = Net()
+print(net)
