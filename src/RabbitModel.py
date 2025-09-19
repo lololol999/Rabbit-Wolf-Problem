4626ee8ec1eede6dee8b3534b8883052db037bdf
lololol999 <stepanbaranov999@gmail.com>
2025-09-20 00:00:47 +0800

    AI base

--- src/RabbitModel.py
+++ src/RabbitModel.py
@@ -1,12 +1,17 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
+
+# нейросеть и слои
 class Net(nn.Module):
    def __init__(self):
        super(Net, self).__init__()
-       self.fc1 = nn.Linear(28 * 28, 200)
-       self.fc2 = nn.Linear(200, 200)
-       self.fc3 = nn.Linear(200, 10)
+       self.fc1 = nn.Linear(5, 10)
+       self.fc2 = nn.Linear(10, 10)
+       self.fc3 = nn.Linear(10, 10)
+       self.fc4 = nn.Linear(10, 6)
+       self.fc5 = nn.Linear(6, 2)
 
    def forward(self, x):
        x = F.relu(self.fc1(x))
@@ -16,3 +21,9 @@ class Net(nn.Module):
 
 net = Net()
 print(net)
+
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+
+criterion = nn.NLLLoss()
+
+# тренировочный цикл: TODO
